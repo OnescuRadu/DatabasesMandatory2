3c4635a15d07f66978ff326a64a5fa79d5903520
Chris <szabo.krisztian.87@gmail.com>
2021-05-29 12:04:20 +0200

    Implemented swagger in the project

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 const router = require('express').Router({ mergeParams: true });
 const passport = require('passport');
-const { isAuthenticated } = require('../middleware');
 const { userController } = require('../controllers')
 
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api
+ * @group foo - Operations about user
+ * @param {string} email.query.required - username or email - eg: user@domain
+ * @param {string} password.query.required - user's password.
+ * @returns {object} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
 router.get('/login', (req, res) => {
   res.send({ response: 'Login failed' });
 });
