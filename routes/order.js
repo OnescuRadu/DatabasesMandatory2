622c09dc7ab76cd5f37f4ad183a0bf01ea43b36b
Radu Onescu <radu@html24.net>
2021-05-30 16:04:58 +0200

    Added swagger docs for multiple routes
    Routes included: /order, /product-group, /product-image, /product-description, /product-rating

--- routes/order.js
+++ routes/order.js
@@ -2,16 +2,51 @@ const router = require('express').Router({ mergeParams: true });
 const { isAuthenticated, hasRole } = require('../middleware');
 const { orderController } = require('../controllers');
 
+/**
+ * Get order by id - Authentication required
+ * @route GET /order/{id}
+ * @group order - Order routes
+ * @param {string} id.path.required - Order id to look up
+ * @returns {object} 200 - Order object
+ * @returns {Error} 401 - User not authenticated
+ * @returns {Error} 404 - Order not found
+ * @security cookie
+ */
 router.get('/:id', isAuthenticated, (req, res, next) => {
-    orderController.getById(req.params.id, id)
-        .then(order => res.json({ response: order }))
+    orderController
+        .getById(req.user, req.params.id)
+        .then((order) => res.json({ response: order }))
         .catch(next);
 });
 
-router.post('/', hasRole("User"), (req, res, next) => {
-    orderController.createOrders(req.user, req.body)
-        .then(orders => res.json({ response: orders.length === 1 ? orders[0] : orders }))
+/**
+ * Create new order - User role required
+ * @route POST /order/
+ * @group order - Order routes
+ * @param {OrderData.model} OrderData.body.required - The order data
+ * @returns {object} 200 - The newly created order object
+ * @returns {Error}  500 - Internal server error
+ * @returns {Error}  401 - Not Authorized
+ * @returns {Error}  401 - Not Authorized to view this order
+ * @returns {Error}  400 - Missing required fields
+ * @security cookie
+ */
+router.post('/', hasRole('User'), (req, res, next) => {
+    orderController
+        .createOrders(req.user, req.body)
+        .then((orders) => res.json({ response: orders.length === 1 ? orders[0] : orders }))
         .catch(next);
 });
+
+/**
+ * @typedef OrderData
+ * @property {Array.<OrderProduct>} items.required - The order's items
+ */
+
+/**
+ * @typedef OrderProduct
+ * @property {string} sellerProductId.required - The
+ * @property {integer} quantity.required - The order's product quantity
+ */
 
 module.exports = router;
--- routes/productRating.js
+++ routes/productRating.js
@@ -2,6 +2,13 @@ const router = require('express').Router({ mergeParams: true });
 const { isAuthenticated } = require('../middleware');
 const { productRatingController } = require('../controllers');
 
+/**
+ * Get Product Rating by ID
+ * @route GET /product-rating/{id}
+ * @group productRating - Product Rating routes
+ * @param {string} id.path.required - Product Rating id to look up
+ * @returns {object} 200 - Product Rating object
+ */
 router.get('/:id', (req, res, next) => {
     productRatingController
         .getProductRatingById(req.params.id)
@@ -9,6 +16,13 @@ router.get('/:id', (req, res, next) => {
         .catch(next);
 });
 
+/**
+ * Get All Product Ratings by Product ID
+ * @route GET /product-rating/product/{id}
+ * @group productRating - Product Rating routes
+ * @param {string} id.path.required - Product id to look up
+ * @returns {Array.object} 200 - Array of Product Ratings objects
+ */
 router.get('/product/:id', (req, res, next) => {
     productRatingController
         .getAllProductRatingsByProductId(req.params.id)
@@ -16,6 +30,17 @@ router.get('/product/:id', (req, res, next) => {
         .catch(next);
 });
 
+/**
+ * Create new product rating - Authentication required
+ * @route POST /product-rating/
+ * @group productRating - Product Rating routes
+ * @param {ProductRatingData.model} ProductRatingData.body.required - The product rating data
+ * @returns {object} 200 - The newly created product rating object
+ * @returns {Error}  500 - Internal server error
+ * @returns {Error}  401 - Not Authorized
+ * @returns {Error}  400 - Missing required fields
+ * @security cookie
+ */
 router.post('/', isAuthenticated, (req, res, next) => {
     productRatingController
         .createProductRating(req.body, req.user)
@@ -23,6 +48,18 @@ router.post('/', isAuthenticated, (req, res, next) => {
         .catch(next);
 });
 
+/**
+ * Update product rating data - Authentication required
+ * @route PUT /product-rating/{id}
+ * @group productRating - Product Rating routes
+ * @param {string} id.path.required - The product rating's id which is to be updated
+ * @param {ProductRatingData.model} ProductRatingData.body.required - The product rating data
+ * @returns {object} 200 - The updated product rating object
+ * @returns {Error}  500 - Internal server error
+ * @returns {Error}  401 - Not Authorized
+ * @returns {Error}  400 - Missing required fields
+ * @security cookie
+ */
 router.put('/:id', isAuthenticated, (req, res, next) => {
     productRatingController
         .updateProductRating(req.params.id, req.body, req.user)
@@ -30,11 +67,28 @@ router.put('/:id', isAuthenticated, (req, res, next) => {
         .catch(next);
 });
 
+/**
+ * Delete product rating by id - Authentication required
+ * @route DELETE /product-rating/{id}
+ * @group productRating - Product Rating routes
+ * @param {string} id.path.required - The product rating's id which is to be deleted
+ * @returns {object} 200 - The deleted manufacturer object
+ * @returns {Error}  500 - Internal server error
+ * @returns {Error}  401 - Not Authorized
+ * @security cookie
+ */
 router.delete('/:id', isAuthenticated, (req, res, next) => {
     productRatingController
         .deleteProductRating(req.params.id, req.user)
         .then((productRating) => res.json({ response: productRating }))
         .catch(next);
 });
+
+/**
+ * @typedef ProductRatingData
+ * @property {integer} rating.required - The rating value
+ * @property {string} review.required - The review
+ * @property {string} productId.required - The product id which the image is uploaded for
+ */
 
 module.exports = router;
